4a3ebe20eb8b98f8bfbd7fa66908fceb7c3a8a7b
jtvberg <jtvberg@gmail.com>
2021-10-16 10:46:01 -0500

    chat online

--- src/js/reducers/chats.js
+++ src/js/reducers/chats.js
@@ -30,15 +30,19 @@ function createChatReducer() {
     }
   };
 
-  const activeChats = createReducer(
-    {},
-    {
-      CHATS_SET_ACTIVE_CHAT: (state, action) => {
-        const { chat } = action;
-        state[chat.id] = chat;
-      },
+  const activeChats = createReducer({}, {
+    'CHATS_SET_ACTIVE_CHAT': (state, action) => {
+      const { chat } = action;
+      state[chat.id] = chat;
+    },
+    'CHATS_UPDATE_USER_STATE': (state, action) => {
+      const { user, chatId } = action;
+      const joinedUsers = state[chatId].joinedUsers;
+      const index = joinedUsers.findIndex(ju => ju.uid === user.uid);
+      if (index < 0 || joinedUsers[index].state === user.state) { return state; }
+      joinedUsers[index].state = user.state;
     }
-  );
+  });
 
   return combineReducers({
     joined,
